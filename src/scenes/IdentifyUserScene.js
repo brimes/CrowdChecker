3317b6bdcdd471b16158ef1ef98b23d658ed8443
Charley Oliveira <charleyoliveira@Charleys-MacBook-Pro.local>
2017-07-05 21:32:48 -0300

    Subindo alteracoes

--- src/scenes/IdentifyUserScene.js
+++ src/scenes/IdentifyUserScene.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { setTheme, MKTextField, MKColor } from 'react-native-material-kit';
 import { Button, COLOR } from 'react-native-material-ui';
 import { Actions } from 'react-native-router-flux';
+import ResponsiveImage from 'react-native-responsive-image';
 
 // Models
 const UserModel = require('../models/UserModel')
@@ -13,34 +14,40 @@ setTheme({
   accentColor: MKColor.Amber,
 });
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',   
-  },
-  title: {
-    fontSize: 32,
-    margin: 10
-  },
-  information: {
-    fontSize: 20,
-    fontStyle: 'italic',
-    textAlign: 'center',
-    margin: 10,
-    marginBottom: 20
+    //alignItems: "center",
+    justifyContent: "center"
   },
   textfield: {
-    width: 300,
-    marginBottom: 20
+    height: 28,  // have to do it on iOS
+    marginTop: 32,
   },
-  button: {
-    alignSelf: 'flex-end',
-    marginRight: 24
-  },
+  textfieldWithFloatingLabel: {
+    height: 48,  // have to do it on iOS
+    marginTop: 10,
+  }
 });
+
+const Textfield = MKTextField.textfield()
+.withPlaceholder('Text...')
+.withStyle(styles.textfield)
+.withTextInputStyle({flex: 1})
+.build();
+
+const TextfieldWithFloatingLabel = MKTextField.textfieldWithFloatingLabel()
+.withPlaceholder('Number...')
+.withStyle(styles.textfieldWithFloatingLabel)
+.withTextInputStyle({flex: 1})
+.withFloatingLabelFont({
+  fontSize: 10,
+  fontStyle: 'italic',
+  fontWeight: '200',
+})
+.withKeyboardType('numeric')
+.build();
+
 
 const TextfieldEmail = MKTextField.textfieldWithFloatingLabel()
   .withFloatingLabelFont({
@@ -84,24 +91,25 @@ class IdentifyUserScene extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          <Image source={require('../../assets/logo.png')} />
-        </Text>
-        <Text style={styles.information}>
+        <View style={{flex:1, alignItems:"center", maxHeight: 150}}>
+        <ResponsiveImage defaultSource={require('../assets/logo.png')} initWidth="138" initHeight="138"/>
+        <Text>
           Esse é o seu primeiro acesso então precisamos saber quem é você.
         </Text>
-        <TextfieldEmail 
+        </View>
+        <View>
+          <TextfieldWithFloatingLabel ref="defaultInput"/>
+        </View>
+        <TextfieldEmail
           placeholder="Digite aqui o seu email"
           autoFocus={true}
           onChangeText={(email) => this.setState({email})}/>
-        <View style={styles.button}>
           <Button
             raised
             onPress={() => this.validateEmail()}
             text={this.state.buttonText}
             disabled={this.state.isButtonDisabled}
           />
-        </View>
       </View>
     )
   }
